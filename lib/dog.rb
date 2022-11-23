fe86b3a94f144f483a391367f6bf6d3298710826
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2022-11-23 10:33:00 -0500

    commit tree 2711c6a3f076e9cdfddac18f09e063ec182ef6c6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
 
+  #setter methods
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  #getter method
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
 end
